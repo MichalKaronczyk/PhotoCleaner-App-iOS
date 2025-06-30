822bfdad6d58efc7b482e80e9264070e8aadd7a2
Michał Karonczyk <mikhail.karonchyk@infotech.edu.pl>
2025-06-30 08:59:54 +0200

    Initial Commit

---
+++ PhotoCleanerTests/PhotoCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerTests.swift
+//  PhotoCleanerTests
+//
+//  Created by Michał Karonczyk on 30/06/2025.
+//
+
+import Testing
+@testable import PhotoCleaner
+
+struct PhotoCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
